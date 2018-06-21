5c7e7f81faa3bed33a3473646b8011862478279e
yassine akkari <joudakkari2015@gmail.com>
2018-06-21 22:25:15 +0100

    server-setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express'); // Fast, unopinionated, minimalist web framework for node.
+const app = express(); // Initiate Express Application
+const mongoose = require('mongoose'); // Node Tool for MongoDB
+const config = require('./config/database'); // Mongoose Config
+const path = require('path'); // NodeJS Package for file paths
+
+// Database Connection
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
+  if (err) {
+    console.log('Could NOT connect to database: ', err);
+  } else {
+    console.log('Connected to database: ' + config.db);
+  }
+});
+
+// Provide static directory for frontend
+app.use(express.static(__dirname + '/client/dist/'));
+
+// Connect server to Angular 2 Index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
+});
+
+
+  
+  // Start Server: Listen on port 8088
+  app.listen(8088, () => {
+    console.log('Listening on port 8088');
+  });
